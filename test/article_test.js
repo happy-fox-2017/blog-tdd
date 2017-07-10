ad18ac1d67785331dee992965b7f534963f89bf2
erwinwahyura <you@example.com>
2017-07-10 16:38:12 +0700

    add some simple test driven development

---
+++ test/article_test.js
@@ -0,0 +1,142 @@
+// process.env.NODE_ENV = 'test';
+const chai = require('chai')
+const chaiHttp = require('chai-http')
+chai.use(chaiHttp)
+
+var should = chai.should();
+var article = require('../models/article')
+var server = require('../app')
+
+describe('article GET', function() {
+  beforeEach(function(done) {
+    var createArticle = new article({
+      title: "test 123",
+      body: "test 123",
+      createdby: "123"
+    })
+    createArticle.save(() => {
+      done()
+    })
+  })
+
+  afterEach(function(done) {
+    article.remove({}, function(err) {
+      done()
+    })
+  })
+
+  describe('get all articles', function() {
+    it('it should get all articles ', function(done) {
+      chai.request(server)
+      .get('/api/articles')
+      .end(function(err, res) {
+        res.body.should.be.a("array");
+     	  res.body.length.should.equal(1);
+        res.should.have.status(200);
+        done()
+      })
+    })
+  })
+})
+
+  describe('post some articles', function() {
+    it('it should create new an articles ', function(done) {
+      //done bersifat asynchronus
+      //kalo tidak ada berfifat synchronus
+      chai.request(server)
+      .post('/api/articles')
+      .send({
+        title: "post",
+        body: "post",
+        createdby: "post"
+      })
+      .end(function(err, res) {
+        res.should.have.status(200)
+     		res.body.should.be.a("object")
+        res.body.should.have.property("title")
+        res.body.should.have.property("body")
+        res.body.should.have.property("createdby")
+        res.body.title.should.equal("post")
+        res.body.body.should.equal("post")
+        res.body.createdby.should.equal("post")
+        done()
+      })
+    })
+  })
+
+describe('article remove', function() {
+  beforeEach(function(done) {
+    var createArticle = new article({
+      title: "delete",
+      body: "delete body",
+      createdby: "delete created"
+    })
+    createArticle.save(() => {
+      done()
+    })
+  })
+
+  afterEach(function(done) {
+    article.remove({}, function(err) {
+      done()
+    })
+  })
+  describe('delete one article', function() {
+    it('it should delete a single article', function(done) {
+      chai.request(server)
+      .get('/api/articles')
+      .end(function(err, res1) {
+        chai.request(server)
+        .delete('/api/articles/'+res1.body[0]._id)
+        .end(function(err, res){
+          res.should.have.status(200);
+          res.body.should.be.a('object');
+          done();
+        })
+      })
+    })
+  })
+})
+
+describe('article Update', function() {
+  var id
+  beforeEach(function(done) {
+    var createArticle = new article({
+      title: "mocha-testing",
+      body: "how to learn mocha",
+      createdby: "erwin"
+    })
+    createArticle.save((err, result) => {
+      if(err) {
+        console.log(err);
+      } else {
+        id = result._id
+        done()
+      }
+    })
+  })
+
+  afterEach(function(done) {
+    article.remove({}, function(err) {
+      done()
+    })
+  })
+  describe('put an article', function() {
+      it('it should edit a single article', function(done) {
+      chai.request(server)
+      .put(`/api/articles/${id}`)
+      .send({title: "Spider",
+             body: "Man",
+             createdby: "homecoming"})
+      .end(function(err, res){
+        res.should.have.status(200);
+        res.body.should.be.a("object");
+        res.body.should.have.property("title");
+        res.body.should.have.property("body");
+        res.body.should.have.property("createdby");
+        done();
+      })
+    })
+
+  })
+})
